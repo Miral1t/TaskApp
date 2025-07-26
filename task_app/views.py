b24e29c294acee807e728e468941a247fdb0adcb
Miral1t <stankourij17@gmail.com>
2025-07-26 12:34:27 +0300

    Task2.0

--- task_app/views.py
+++ task_app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Task
+class TaskListView(ListView):  # type: ignore
+    model = Task
+    template_name = 'task_list.html'
+    context_object_name = 'task'
 
-# Create your views here.
